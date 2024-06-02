8585a89b048f0c9ccc13dd70dc62ba3ab3b54db9
Jefri Jaya <jefrijaya08@gmail.com>
2024-06-02 15:27:40 +0700

    Update lambang.js

--- constanst/lambang.js
+++ constanst/lambang.js
@@ -1,17 +1,17 @@
 export const lambang = [
   {
-    title: "Warna Biru, Hijau, Orange",
+    title: "Bentuk Lingkaran:",
     description: [
       {
-        list: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere mollitia aperiam quaerat accusamus! Sunt reiciendis labore iure qui molestias rerum, delectus illo esse iusto nam, quos nobis. Nobis, repudiandae!",
+        list: "Lingkaran dalam logo ini sering kali melambangkan kesatuan, kontinuitas, dan globalisasi. Ini bisa menggambarkan persatuan pemuda Indonesia dalam mencapai tujuan ekonomi yang berkelanjutan.",
       },
     ],
   },
   {
-    title: "Tulisan IYEL",
+    title: "Warna Hijau dan Biru:",
     description: [
       {
-        list: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere mollitia aperiam quaerat accusamus! Sunt reiciendis labore iure qui molestias rerum, delectus illo esse iusto nam, quos nobis. Nobis, repudiandae!",
+        list: "Warna hijau sering dikaitkan dengan pertumbuhan, alam, dan keberlanjutan, sementara warna biru melambangkan kepercayaan, stabilitas, dan tanggung jawab. Kombinasi ini menunjukkan fokus IYEL pada pertumbuhan ekonomi yang berkelanjutan dan bertanggung jawab.",
       },
     ],
   },
